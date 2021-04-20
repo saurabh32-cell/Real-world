c91ec74019e4df5bba0ab075847aa053bed4bbc4
saurabh32-cell <saurabh32-cell>
2021-04-20 13:51:26 +0545

    final project

---
+++ fashion/whole/forms.py
@@ -0,0 +1,12 @@
+# from django import forms
+# from .models import *
+# from django.forms import ModelForm
+#
+#
+#
+#
+# class SubmitForm(ModelForm):
+#     class Meta:
+#         model=SubmitForm
+#         fields="__all__"
+
